0788d90e2ad70fc7e500e6c5b4e8ef9f0ef29c58
Siron22 <s0949528587@gmail.com>
2023-11-22 15:57:28 +0100

    update pages and endpoints

--- pages/endpoints.py
+++ pages/endpoints.py
@@ -1,8 +1,18 @@
+import pytest
 
 
 class EndPoints:
     BASE_PAGE = "http://selenium1py.pythonanywhere.com/"
     LOGIN_PAGE = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    ITEM_PAGE = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    ITEM_PAGE = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     NEW_ITEM_PAGE = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-
+    PROMO_LINKS = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
--- pages/item_page.py
+++ pages/item_page.py
@@ -10,29 +10,40 @@ class ItemPageLocators:
     ALERT_SHIPPING = (By.CSS_SELECTOR, "div.alert:nth-child(2)")
     ALERT_BASKET_TOTAL = (By.CSS_SELECTOR, "div.alert:nth-child(3)")
     BUTTON_VIEW_BASKET = (By.CSS_SELECTOR, "a.btn-info:nth-child(1)")
-
+    ITEM_NAME = (By.CSS_SELECTOR, "div.product_main>h1")
+    ALERT_NAME = (By.XPATH, "//div[1]/div[1]/strong")
 
 
 class ItemPage(BasePage):
+
     def add_item_into_basket(self):
         add_button = self.browser.find_element(*ItemPageLocators.BUTTON_ADD_TO_BASKET)
         add_button.click()
 
     def should_item_added(self):
-        self.should_add_allert()
-        self.should_shipping_allert()
-        self.should_allert_basket_total()
+        self.should_add_alert()
+        self.should_shipping_alert()
+        self.should_alert_basket_total()
         self.should_button_view_basket()
+        self.should_item_name_add()
 
-    def should_add_allert(self):
+    def should_add_alert(self):
         assert self.is_element_present(*ItemPageLocators.ALERT_ADD_TO_THE_BASKET), \
             "Alert add to the basket is not presented"
 
-    def should_shipping_allert(self):
+    def should_not_add_alert(self):
+        assert self.is_not_element_present(*ItemPageLocators.ALERT_ADD_TO_THE_BASKET), \
+            "Alert add to the basket is presented, but should not"
+
+    def should_add_alert_disappear(self):
+        assert self.is_disappeared(*ItemPageLocators.ALERT_ADD_TO_THE_BASKET), \
+            "Alert add to the basket is disappear"
+
+    def should_shipping_alert(self):
         assert self.is_element_present(*ItemPageLocators.ALERT_SHIPPING), \
             "Alert shipping to the basket is not presented"
 
-    def should_allert_basket_total(self):
+    def should_alert_basket_total(self):
         assert self.is_element_present(*ItemPageLocators.ALERT_BASKET_TOTAL), \
             "Basket total alert  is not presented"
 
@@ -41,7 +52,11 @@ class ItemPage(BasePage):
             "Button view basket is not presented"
 
 
-
+    def should_item_name_add(self):
+        item_name = self.browser.find_element(*ItemPageLocators.ITEM_NAME).text
+        alert_name = self.browser.find_element(*ItemPageLocators.ALERT_NAME).text
+        assert item_name == alert_name, \
+            "Name in the basket is not correct"
 
 
 
